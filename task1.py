7283a9c6cee22edf68a7303a8b9cee55fe0ae4d2
Daria Milyakova <103563978+GithubAgitator@users.noreply.github.com>
2024-11-13 08:54:28 +0300

    tasks1/3

--- task1.py
+++ task1.py
@@ -6,3 +6,35 @@
 
 # Vector2D(1, 2) + Vector2D(2, 3) = Vector2D(3, 5)
 # Vector2D(2, 3) * 3 = Vector2D(6, 9)
+
+class Vector2D:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    def __add__(self, other):
+        t1 = self.a + other.a
+        t2 = self.b + other.b
+        return Vector2D(t1, t2)
+
+    def __sub__(self, other):
+        t1 = self.a - other.a
+        t2 = self.b - other.b
+        return Vector2D(t1, t2)
+
+    def __mul__(self, other=3):
+        t1 = self.a * other
+        t2 = self.b * other
+        return Vector2D(t1, t2)
+
+
+
+    def __repr__(self):
+        return f"Vector2D {self.a}, {self.b}"
+
+a = Vector2D(1, 2)
+a2 = Vector2D(2, 3)
+print(a + a2)
+print(a - a2)
+print(a * 3)
+print(a2 * 3)
--- task3.py
+++ task3.py
@@ -3,3 +3,22 @@
 
 # Angle(130) + Angle(50) = Angle(0)
 # Angle(50) - Angle(130) = Angle(80)
+
+class Angle():
+    def __init__(self, a):
+        self.a = a
+
+    def __add__(self, other):
+        return Angle(self.a + other.a)
+
+
+    def __sub__(self, other):
+        return Angle(self.a - other.a)
+
+    def __repr__(self):
+        return f"Angle({self.a})"
+
+ygol = Angle(130)
+ygol2 = Angle(50)
+print(ygol + ygol2)
+print(ygol - ygol2)
