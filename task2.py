56e371ee0f1eace427e4f6cfb9332a83c52bdb8c
Daria Milyakova <103563978+GithubAgitator@users.noreply.github.com>
2024-11-13 21:40:23 +0300

    tasks2

--- task2.py
+++ task2.py
@@ -3,3 +3,34 @@
 # чтобы можно было складывать размеры файлов,
 # вычитать их и умножать на целое число для вычисления объема
 # нескольких файлов одинакового размера.
+
+class FileSize:
+    def __init__(self, size):
+        self.size = size
+
+    def __add__(self, other):
+        if other.size < 0 or self.size < 0:
+            return 'Не верный размер'
+        return FileSize(self.size + other.size)
+
+    def __sub__(self, other):
+        if other.size < 0 or self.size < 0:
+            return 'Не верный размер'
+        r = (self.size - other.size)
+        if r < 0:
+            return 'Недостаточно места'
+        return FileSize(r)
+
+    def __mul__(self, other):
+        if other < 0 or self.size < 0:
+            return 'Не верный размер'
+        return FileSize(self.size * other)
+    def __repr__(self):
+        return f'{self.size}'
+
+size1 = FileSize(5)
+size2 = FileSize(10)
+print(size1 + size2)
+print(size1 - size2)
+print(size1 * -2)
+print(size2 * 8)
